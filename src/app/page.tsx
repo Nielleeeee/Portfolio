3876953c7c9aaacb16398fe3a58cb4e2085712e8
Nielleeeee <jandnllplz@gmail.com>
2023-08-04 22:44:10 +0800

    portfolio init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-4xl font-semibold text-center mt-[120px] mx-auto w-full">Hi, I&apos;m Jan Danielle Plaza</h1>
+    </main>
+  )
+}
