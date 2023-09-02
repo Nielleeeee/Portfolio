5af4c85b5fbd305caae86a76d0f3e77717e83e9c
Nielleeeee <jandnllplz@gmail.com>
2023-09-02 18:35:59 +0800

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} bg-white scroll-smooth`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
