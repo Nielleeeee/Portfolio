81ab7bd39f789a67f71937669b43cdad85cdb2da
Nielleeeee <jandnllplz@gmail.com>
2024-02-02 20:57:26 +0800

    refactor: changed tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      colors: {
+        white: "#FAFAFA",
+        black: "#2B2B2B",
+        mainbg: "#FFB880",
+        primary: "#FF9D4F",
+        secondary: "#FF770B",
+        tertiary: "#FFD889",
       },
     },
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
-    },
-    colors: {
-      'white': '#FAFAFA',
-      'black': '#2B2B2B',
-      'mainbg': '#FFB880',
-      'primary': '#FF9D4F',
-      'secondary': '#FF770B',
-      'tertiary': '#FFD889',
-    }
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
