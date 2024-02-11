677b3c64dc98391e10e5396c5a11ffcb1a56da95
Nielleeeee <jandnllplz@gmail.com>
2024-02-12 02:42:36 +0800

    chore: changed background of project section

--- src/app/(landingPage)/projects.tsx
+++ src/app/(landingPage)/projects.tsx
@@ -5,44 +5,71 @@ import { performRequest } from "@/lib/datocms";
 import ProjectList from "@/components/projectList";
 
 const PAGE_CONTENT_QUERY = `
-  query Project {
-    allPersonalProjects(orderBy: _createdAt_ASC) {
-      name
-      description
-      projectStatus
-      link
-      image {
-        responsiveImage {
-            srcSet
-            webpSrcSet
-            sizes
-            src
-            width
-            height
-            aspectRatio
-            alt
-            title
-            base64
-          }
+query Project {
+  allPersonalProjects(orderBy: _createdAt_ASC) {
+    name
+    description
+    link
+    image {
+      responsiveImage {
+        srcSet
+        webpSrcSet
+        sizes
+        src
+        width
+        height
+        aspectRatio
+        alt
+        title
+        base64
       }
     }
-  }`;
+  }
+  allWorkProjects(orderBy: _createdAt_ASC) {
+    name
+    description
+    link
+    image {
+      responsiveImage {
+        srcSet
+        webpSrcSet
+        sizes
+        src
+        width
+        height
+        aspectRatio
+        alt
+        title
+        base64
+      }
+    }
+  }
+}`;
 
 export default async function Projects() {
   const {
-    data: { allPersonalProjects },
+    data: { allPersonalProjects, allWorkProjects },
   } = await performRequest({ query: PAGE_CONTENT_QUERY });
 
   return (
-    <section id="projects" className="w-full h-auto bg-white py-10">
+    <section id="projects" className="w-full h-auto bg-white py-10 bg-[url('/endless-constellation.png')] bg-no-repeat bg-center bg-fixed bg-cover">
       <Transition>
         <MainContainer>
-          <div className="py-0 lg:py-10">
+          <div className="py-5 lg:py-10">
             <h2 className="w-full text-center text-secondary font-bold text-3xl mb-10">
               Personal Projects
             </h2>
             <div className="w-full flex flex-col gap-4 md:gap-8">
               <ProjectList projectList={allPersonalProjects} />
+            </div>
+          </div>
+
+          <div className="py-5 lg:py-10">
+            <h2 className="w-full text-center text-secondary font-bold text-3xl mb-10">
+              Work Projects
+            </h2>
+            <div className="w-full flex flex-col gap-4 md:gap-8">
+              <ProjectList projectList={allWorkProjects} />
             </div>
           </div>
         </MainContainer>
