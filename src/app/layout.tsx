d34bdac0c45189aa9237cf4a3cb53e5b61251b27
jd.plaza <jan.plaza@oqulo.com>
2024-02-24 00:00:58 +0800

    chore: added speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-white scroll-smooth`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
