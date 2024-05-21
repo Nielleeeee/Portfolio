17250a68ee65cec0270ce6c8632cbd6de881488c
Nielleeeee <jandnllplz@gmail.com>
2024-05-22 00:26:45 +0800

    feat: added hotjar

---
+++ src/lib/hotjar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Hotjar() {
+  return (
+    <script>
+      {`
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:4989775,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+    </script>
+  );
+}
