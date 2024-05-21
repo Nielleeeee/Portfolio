8a0e64d488d5f8c5e724af988a2d6d53cc79c722
Nielleeeee <jandnllplz@gmail.com>
2024-05-22 00:27:00 +0800

    feat: added provider in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import Providers from "@/lib/provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} bg-white scroll-smooth`}>
-        {children}
-        <Analytics />
-        <SpeedInsights />
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
