d73ccb5a65e55d5aa2fecd3c6bfd27fab17f1ca6
Nielleeeee <jandnllplz@gmail.com>
2024-05-25 05:37:37 +0800

    refactor: changed script tag in hotjar component

--- src/lib/hotjar.tsx
+++ src/lib/hotjar.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import Script from "next/script";
 
 export default function Hotjar() {
   return (
-    <script>
+    <Script id="hotjar">
       {`
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:4989775,hjsv:6};
+        h._hjSettings={hjid:${process.env.HOTJAR_ID},hjsv:${process.env.HOTJAR_VERSION}};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
-    </script>
+    </Script>
   );
 }
